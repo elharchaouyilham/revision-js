0c2a5697a30d2239e3c7106d53e1b9a2e0f99871
elharchaouyilham <elharchaouyilham77@gmail.com>
2026-03-30 12:13:05 +0200

    oop&php

---
+++ oop&php/ex.php
@@ -0,0 +1,14 @@
+<?php
+$prenom="ilham";
+$age=22;
+echo "bonjour je m'appelle $prenom et j'ai $age ans";
+if($age>=18){
+    echo "majeur";
+}else {
+    echo "mineur";
+}
+
+for($i=0;$i<=10;$i++){
+    echo $i ."/n";
+}
+?>
