d61717e98078459402f3a1150bb20b6d2e04f67a
elharchaouyilham <elharchaouyilham77@gmail.com>
2026-04-06 18:20:58 +0200

    oop

---
+++ OOP MARDI/Produit.php
@@ -0,0 +1,53 @@
+<?php
+class Produit{
+    private $nom;
+    private $prix;
+    private $stock;
+    public function __construct($nom,$prix,$stock)
+    {
+     $this->nom=$nom;
+     $this->prix=$prix;
+     $this->stock=$stock;
+    }
+     public function getNom(){
+      return $this->nom;
+     }
+     public function getPrix(){
+      return $this->prix;
+     }
+     public function getstock(){
+      return $this->stock;
+     }
+    public function setNom($nom){
+     $this->nom=$nom;
+     }
+    public function setPrix($prix){
+       $this->prix=$prix;  
+    }
+    public function setSock($stock){ 
+        if($stock>0){
+       $this->stock=$stock; 
+       }else{
+        echo "le stock ne doit pas etre negatif";
+       }
+    }
+    public function afficher(){
+        echo $this->nom."".$this->prix."€".$this->stock;
+    }
+
+
+}
+$produit1=new Produit("produit1",30,67);
+$produit2=new Produit("produit2",23,45);
+
+
+// créez produit.php et déclarez la classe Produit avec les 3 attributs private : $nom, $prix,
+// $stock
+// 2. Ajoutez le constructeur __construct($nom, $prix, $stock) qui initialise les 3 attributs
+// 3. Ajoutez les getters : getNom(), getPrix(), getStock()
+// 4. Ajoutez le setter setPrix($prix) avec validation : refuser les prix négatifs
+// 5. Ajoutez le setter setStock($stock) avec validation : refuser un stock < 0
+// 6. Ajoutez la méthode afficher() qui affiche : "[nom] — [prix]€ (stock : [stock])"
+// 7. Instanciez 2 produits différents et appelez afficher() sur chacun
+// 8. Testez setPrix(-5) — vérifiez que le message d'erreur s'affiche
+// 9. Testez getPrix() et getNom() — vérifiez que les valeurs correctes sont retournées
